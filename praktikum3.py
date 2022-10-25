0b9da588cf426f4c3f1d42223d26c8bb805895e3
mdinalayubi <mdinalayubi@gmail.com>
2022-10-25 17:18:37 +0700

    pembuatan file praktikum

---
+++ praktikum3.py
@@ -0,0 +1,13 @@
+print("program Python Hitung Luas dan Keliling Lingkaran")
+print("Nama : Muhammad Din Al Ayubi")
+print("NIM : 312210293")
+print("Kelas : T.I 22 A.3")
+
+pi = 22/7
+jari = float(input("Masukkan jari- jari : "))
+luas = pi*(jari*jari)
+keliling = 2*pi*jari
+
+print("\n-------------------Hasilnya--------------")
+print("Luas Lingkaran =","{:.3f}".format(luas))
+print("Keliling Lingkaran =","{:.3f}".format(keliling))
